3fe1aa9921efe70c473187bbca90ba99e5dc9a91
Akifer23 <134030941+Akifer23@users.noreply.github.com>
2023-12-05 12:13:09 -0600

    Add files via upload

---
+++ js/script_contacto.js
@@ -0,0 +1,45 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_hula69t';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('mensaje enviado con ecito :)!');
+        document.getElementById('form').reset(); // Limpiar el formulario
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
+
+let form = document.getElementById('container');
+let idleTime = 0;
+const idleInterval = 1000; // 1 segundo
+
+function resetIdleTime() {
+    idleTime = 0;
+}
+
+function checkIdleTime() {
+    idleTime += 1;
+    if (idleTime > 60) { // Cambia el valor según la cantidad de segundos que deseas para la inactividad
+        form.classList.add('flip');
+        setTimeout(() => {
+            form.classList.remove('flip');
+        }, 3000); // Ajusta el tiempo de la animación de vuelta (3 segundos en este ejemplo)
+        resetIdleTime();
+    }
+}
+
+document.addEventListener('mousemove', resetIdleTime);
+document.addEventListener('keypress', resetIdleTime);
+
+setInterval(checkIdleTime, idleInterval);
